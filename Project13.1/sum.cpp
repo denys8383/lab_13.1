285af7196de12f30b5283d8aad176d1f1eef7f13
denys8383 <Denys.Barchuk.IK.2023@lpnu.ua>
2023-11-21 17:16:38 +0200

    commit 13.1

---
+++ Project13.1/sum.cpp
@@ -0,0 +1,16 @@
+﻿#include <math.h>
+#include "dod.h"
+#include "sum.h"
+#include "var.h" // підключили зовнішні оголошення змінних
+using namespace nsDod;
+using namespace nsVar;
+void nsSum::sum() {
+	n = 0;
+	a = x;
+	s = 0;
+	do {
+		n++;
+		dod(); // виклик процедури обчислення доданку
+		s += exp(-x);
+	} while (fabs(a) < e);
+}
